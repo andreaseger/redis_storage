e3e4c828e3ff3123901469963aba1e575eb53a3f
sch1zo <dev@eger-andreas.de>
2011-06-24 23:50:57 +0200

    updated README

--- lib/redis_storage/railtie.rb
+++ lib/redis_storage/railtie.rb
@@ -1,14 +1,13 @@
 require 'redis_storage'
-#require 'rails'
 module RedisStorage
   class Railtie <::Rails::Railtie
     rake_tasks do
       load "tasks/redis_storage.rake"
     end
-    if ::Rails.version.to_f >= 3.1
-      config.app_generators.orm :redis
-    else
-      config.generators.orm :redis
-    end
+    #if ::Rails.version.to_f >= 3.1
+    #  config.app_generators.orm :redis
+    #else
+    #  config.generators.orm :redis
+    #end
   end
 end
